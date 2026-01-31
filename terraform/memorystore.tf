4c0a5ae8ed21c0ed4ef1787d73f5ef8b982109b4
MisaDevs <misavazquezsifuentes@gmail.com>
2026-01-31 16:03:40 -0600

    Fix terraform fmt in memorystore.tf

--- terraform/memorystore.tf
+++ terraform/memorystore.tf
@@ -14,14 +14,13 @@
 
 # Create the Memorystore (redis) instance
 resource "google_redis_instance" "redis-cart" {
+  # count specifies the number of instances to create;
+  # if var.memorystore is true then the resource is enabled
+  count = var.memorystore ? 1 : 0
+
   name           = "redis-cart"
   memory_size_gb = 1
   region         = var.region
-
-  # count specifies the number of instances to create;
-  # if var.memorystore is true then the resource is enabled
-  count          = var.memorystore ? 1 : 0
-
   redis_version  = "REDIS_7_0"
   project        = var.gcp_project_id
 
@@ -32,14 +31,14 @@ resource "google_redis_instance" "redis-cart" {
 
 # Edit contents of Memorystore kustomization.yaml file to target new Memorystore (redis) instance
 resource "null_resource" "kustomization-update" {
+  # count specifies the number of instances to create;
+  # if var.memorystore is true then the resource is enabled
+  count = var.memorystore ? 1 : 0
+
   provisioner "local-exec" {
     interpreter = ["bash", "-exc"]
     command     = "sed -i \"s/REDIS_CONNECTION_STRING/${google_redis_instance.redis-cart[0].host}:${google_redis_instance.redis-cart[0].port}/g\" ../kustomize/components/memorystore/kustomization.yaml"
   }
-
-  # count specifies the number of instances to create;
-  # if var.memorystore is true then the resource is enabled
-  count          = var.memorystore ? 1 : 0
 
   depends_on = [
     resource.google_redis_instance.redis-cart
